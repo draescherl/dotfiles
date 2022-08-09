51e7e89e4fce6697b4deaaa06545a7d5d2907fdc
Lucas DRAESCHER <lucas.draescher@gmail.com>
2022-08-10 00:01:44 +0200

    Added automatic architecture detection for clever cloud repository

--- install.sh
+++ install.sh
@@ -55,7 +55,7 @@ apt install -yqq sbt
 
 # Clever Cloud CLI
 curl -fsSL https://clever-tools.clever-cloud.com/gpg/cc-nexus-deb.public.gpg.key | gpg --dearmor -o /usr/share/keyrings/cc-nexus-deb.gpg
-echo "deb [signed-by=/usr/share/keyrings/cc-nexus-deb.gpg] https://nexus.clever-cloud.com/repository/deb stable main" | tee -a /etc/apt/sources.list
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/cc-nexus-deb.gpg] https://nexus.clever-cloud.com/repository/deb stable main" | tee -a /etc/apt/sources.list
 apt update -yqq
 apt install -yqq clever-tools
 
