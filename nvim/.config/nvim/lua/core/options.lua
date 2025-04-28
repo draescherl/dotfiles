df5644fb66cbd9480c3e3114f09fa7a6f4ef8f04
Lucas DRAESCHER <lucas.draescher@gmail.com>
2025-04-28 18:43:41 +0200

    nvim: replace kickstart.nvim with my own config

---
+++ nvim/.config/nvim/lua/core/options.lua
@@ -0,0 +1,32 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.showmode = false
+
+vim.opt.list = true
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+
+vim.opt.cursorline = true
+vim.opt.scrolloff = 15
+
+vim.opt.inccommand = "split"
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.signcolumn = "yes"
+
+vim.opt.mouse = "a"
+vim.opt.clipboard = "unnamedplus"
+vim.opt.undofile = true
+vim.opt.updatetime = 250
+
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+vim.opt.tabstop = 4
+vim.opt.expandtab = true
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+
+vim.opt.shm = "S"
+vim.opt_global.shortmess:remove("F")
+vim.opt.conceallevel = 0
---
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -0,0 +1,17 @@
+return {
+	"echasnovski/mini.nvim",
+	version = false,
+	config = function()
+		require("mini.pairs").setup()
+		require("mini.icons").setup()
+		require("mini.files").setup({
+			windows = {
+				preview = true,
+				width_focus = 25,
+				width_preview = 80,
+			},
+		})
+
+		vim.keymap.set("n", "<leader>f", MiniFiles.open)
+	end,
+}
