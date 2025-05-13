e5140d738274a3496623a534a3c706738ba790a9
Lucas DRAESCHER <lucas.draescher@gmail.com>
2025-05-13 14:53:28 +0200

    nvim: add dadbod

--- nvim/.config/nvim/lua/plugins/blink.lua
+++ nvim/.config/nvim/lua/plugins/blink.lua
@@ -19,8 +19,12 @@ return { -- Autocompletion
 			documentation = { auto_show = true, auto_show_delay_ms = 500 },
 		},
 		sources = {
+			per_filetype = {
+				sql = { "dadbod" },
+			},
 			default = { "lsp", "path", "snippets", "lazydev", "buffer" },
 			providers = {
+				dadbod = { module = "vim_dadbod_completion.blink" },
 				lazydev = { module = "lazydev.integrations.blink", score_offset = 100 },
 			},
 		},
