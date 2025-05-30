bfbb5490d82ab08ba98d4c7e53b2eaed29e9a8c8
Lucas DRAESCHER <lucas.draescher@gmail.com>
2025-05-30 18:12:38 +0200

    install: only install items from a given list

--- install.sh
+++ install.sh
@@ -1,3 +1,22 @@
 #!/usr/bin/env bash
 
-stow -v -S --restow */
+items=(
+    "alacritty"
+    "bat"
+    "btop"
+    "fish"
+    "flameshot"
+    "git"
+    "ideavim"
+    "nvim"
+    "starship"
+    "sway"
+    "swaylock"
+    "swaync"
+    "tofi"
+    "waybar"
+    "wezterm"
+    "wlogout"
+)
+
+stow -v -S --restow "${items[@]}"
