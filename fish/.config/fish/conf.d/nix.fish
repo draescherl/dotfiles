ee530017b69b17375bff00ff4380906ea030d08f
Lucas DRAESCHER <lucas.draescher@gmail.com>
2025-09-11 10:44:23 +0200

    fish: source nix if installed

--- fish/.config/fish/conf.d/nix.fish
+++ fish/.config/fish/conf.d/nix.fish
@@ -1,3 +1,8 @@
 abbr nixrl sudo nixos-rebuild switch
 abbr nixgc sudo nix-collect-garbage
 abbr nixls sudo nixos-rebuild list-generations
+
+set -l nix_init_script_path /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.fish
+if test -e $nix_init_script_path
+    source $nix_init_script_path
+end
