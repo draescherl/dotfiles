0d1368ddf5073e08b05f0f0d8f54147b1108861d
Lucas DRAESCHER <lucas.draescher@gmail.com>
2025-09-15 01:14:43 +0200

    nvim: add git blame plugin

---
+++ nvim/.config/nvim/lua/plugins/blame.lua
@@ -0,0 +1,10 @@
+return {
+	{
+		"FabijanZulj/blame.nvim",
+		lazy = false,
+		config = function()
+			require("blame").setup({})
+			vim.keymap.set("n", "<leader>gb", "<cmd>BlameToggle<cr>", {})
+		end,
+	},
+}
