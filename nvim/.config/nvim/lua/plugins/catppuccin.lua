44c802ae38f5bf594d5905dd6942acebdf63458b
Lucas DRAESCHER <lucas.draescher@gmail.com>
2025-09-29 19:10:10 +0200

    nvim: make background transparent

--- nvim/.config/nvim/lua/plugins/catppuccin.lua
+++ nvim/.config/nvim/lua/plugins/catppuccin.lua
@@ -1,9 +1,15 @@
 return {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    lazy = false,
-    config = function()
-        vim.cmd.colorscheme "catppuccin-mocha"
-    end
+	"catppuccin/nvim",
+	name = "catppuccin",
+	priority = 1000,
+	lazy = false,
+	config = function()
+		vim.cmd.colorscheme("catppuccin-mocha")
+		vim.cmd([[
+		    highlight Normal guibg=none
+		    highlight NonText guibg=none
+		    highlight Normal ctermbg=none
+		    highlight NonText ctermbg=none
+		]])
+	end,
 }
